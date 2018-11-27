72c7d0989912944b9b1fd120bc674b43f4671fe4
Azraf Chowdhury <achowdhur@ltu.edu>
2018-11-27 22:52:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,12 +53,12 @@ class Song
     song
   end
   
-  def self.create_from_filename
-     c = self.new 
-    c.name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[1] 
-    c.artist_name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[0]
-    c.save
-    c
+  def self.create_from_filename(mp3_formatted_file)
+    song = self.new 
+    song.name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[1] 
+    song.artist_name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[0]
+    song.save
+    song
   end 
   
   def self.destroy_all
